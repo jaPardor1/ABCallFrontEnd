9399cd7c63dfa296fa645c18a765328a9ed46af3
Jorge Pardo <ja.pardor1@uniandes.edu.co>
2024-10-12 12:53:00 -0500

    avance 1 creacion de formulario registro de pqr

--- ABCall/src/app/pqr/formulario-pqr/formulario-pqr.component.ts
+++ ABCall/src/app/pqr/formulario-pqr/formulario-pqr.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-pqr',
   templateUrl: './formulario-pqr.component.html',
   styleUrl: './formulario-pqr.component.css'
 })
-export class FormularioPqrComponent {
+export class FormularioPqrComponent implements OnInit{
+
+  public formPqr:FormGroup;
+  constructor(private formBuilder:FormBuilder){
+    this.formPqr=this.formBuilder.group({
+      tipoSolicitud:['',{
+       validators:[Validators.required]
+      }],
+      asunto:[''],
+      descripcion:['']
+  });
+  }
+  ngOnInit(): void {
+
+  }
+
+
+
 
 }
