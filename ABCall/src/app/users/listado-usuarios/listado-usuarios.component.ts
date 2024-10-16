05c1bf6753682006669f8a266b193c2862b502e1
jaPardor1 <ja.pardor1@uniandes.edu.co>
2024-10-15 22:31:50 -0500

    creacion de la version inicial para el listado de usuarios registrados (#4)

--- ABCall/src/app/users/listado-usuarios/listado-usuarios.component.ts
+++ ABCall/src/app/users/listado-usuarios/listado-usuarios.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserDto } from '../user';
 
 @Component({
   selector: 'app-listado-usuarios',
   templateUrl: './listado-usuarios.component.html',
   styleUrl: './listado-usuarios.component.css'
 })
-export class ListadoUsuariosComponent {
+export class ListadoUsuariosComponent  {
+  displayedColumns: string[] = ['identificacion', 'nombre', 'perfil','actions'];
+  usersList:UserDto[];
+
+  constructor() {
+
+    this.usersList = [
+      {
+        identificacion:'5632584',
+        nombre:'Juan Perez',
+        perfil:'Gestor'
+
+      },
+      {
+        identificacion:'155151501',
+        nombre:'Clara perez',
+        perfil:'Gestor'
+
+      },
+      {
+        identificacion:'1032465885',
+        nombre:'Jhon smith',
+        perfil:'Administrador'
+
+      }
+    ]
+  }
+
 
 }
