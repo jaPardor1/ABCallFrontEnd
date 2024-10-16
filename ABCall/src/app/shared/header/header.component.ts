9d76b2447c3acd0a30d6bb81bd807bac06f3df18
jaPardor1 <ja.pardor1@uniandes.edu.co>
2024-10-16 09:48:02 -0500

    se crea la version 1 para la barra de navegacion (#5)

--- ABCall/src/app/shared/header/header.component.ts
+++ ABCall/src/app/shared/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HeaderOptionDTO } from './headerOption';
 
 @Component({
   selector: 'app-header',
@@ -6,9 +7,46 @@ import { Component } from '@angular/core';
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-     public nombreModulo:string='RADICAR PQR'
+  public moduleName: string = 'BIENVENIDO'
+  public options: HeaderOptionDTO[];
+  public currentOption=3;
+  constructor() {
 
+    this.options = [
+      {
+        id: 1,
+        name: 'RADICAR PQR',
+        isActive: false,
+        link:"createIncidence"
+      },
+      {
+        id: 2,
+        name: 'CONSULTAR PQR',
+        isActive: false,
+        link:"listIncidences"
+      },
+      {
+        id: 3,
+        name: 'USUARIOS REGISTRADOS',
+        isActive: true,
+        link:"listUsers"
+      }
+    ]
+  }
 
+  setModuleName(name:string,id:number){
+    this.moduleName = name;
+    this.setActiveOption(id);
 
-
+  }
+  setActiveOption(cOption:number){
+    debugger;
+    //desactivar opcion anterior
+    let searched_obj: HeaderOptionDTO | undefined = this.options.find(i=>i.id==this.currentOption);
+    if(searched_obj!==undefined) searched_obj.isActive = false;
+    // activar opcion seleccionada
+    searched_obj = this.options.find(i=>i.id==cOption);
+    if(searched_obj!==undefined) searched_obj.isActive = true;
+    this.currentOption=cOption;
+  }
 }
